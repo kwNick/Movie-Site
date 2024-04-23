32b7c33bba2d87a535125c096fe17185a1979e8a
Nickolas Piraino <kwnp17c@gmail.com>
2024-04-23 14:59:58 -0400

    theme setup toggler for light and dark mode using nextTheme and shadcn darkmode

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,10 @@ import Image from 'next/image'
 import Link from 'next/link'
 import GenreDropDown from './GenreDropDown'
 import SearchInput from './SearchInput'
+import ThemeToggler from './ThemeToggler'
 const Header = () => {
     return (
-        <div className='w-full flex items-center justify-between p-5 bg-[#121212]'>
+        <div className='w-full flex items-center justify-between backdrop-blur-2xl transition-colors p-5 bg-[#12121280] gap-4 md:gap-0 sticky z-50 top-0'>
             <Link href="/">
                 <Image
                     src="https://i.ibb.co/ZW0SbjJ/logo-dark.png"
@@ -16,6 +17,7 @@ const Header = () => {
             <div className='text-white flex space-x-3 items-center'>
                 <GenreDropDown />
                 <SearchInput />
+                <ThemeToggler />
             </div>
         </div>
     )
