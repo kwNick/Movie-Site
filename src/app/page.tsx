c6e9babcefb2c90c1b94687433c9a680c718cd60
Nickolas Piraino <kwnp17c@gmail.com>
2024-04-25 01:03:30 -0400

    MovieContainer component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import CarouselBanner from "@/components/CarouselBanner";
+import MovieContainer from "@/components/MovieContainer";
 import Image from "next/image";
 
 export default function Home() {
@@ -6,6 +7,9 @@ export default function Home() {
     <main className="w-full min-h-screen">
       <p className="tracking-wider">Movie Streaming Website!!</p>
       <CarouselBanner />
+      <div className="flex flex-col space-y-2">
+        <MovieContainer />
+      </div>
     </main>
   );
 }
