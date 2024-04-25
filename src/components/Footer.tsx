920f37f30cd92d355824b612642d5716984bc0f0
Nickolas Piraino <kwnp17c@gmail.com>
2024-04-24 22:31:10 -0400

    finished footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,67 @@
 import Link from 'next/link'
 import React from 'react'
 import Image from 'next/image'
+type item = {
+    title: string;
+    href: string;
+}
+
+const infoArray = [
+    {
+        title: "About Us",
+        href: "/about",
+    },
+    {
+        title: "Contact Us",
+        href: "/contact",
+    },
+    {
+        title: "Terms & Conditions",
+        href: "/terms",
+    },
+    {
+        title: "Privacy Policy",
+        href: "/privacy",
+    },
+];
+const categoryArray = [
+    {
+        title: "Videos",
+        href: "/videos",
+    },
+    {
+        title: "Gaming",
+        href: "/gaming",
+    },
+    {
+        title: "Travel",
+        href: "/travel",
+    },
+    {
+        title: "Music",
+        href: "/music",
+    },
+    {
+        title: "Sports",
+        href: "/sports",
+    },
+];
+
+const Information = ({ Array }: any) => {
+    return (
+        <div className='flex flex-col text-gray-300'>
+            {Array.map((item: item) => (
+                <Link href={item?.href} key={item?.title} className='hover:text-white text-sm
+                mb-1 cursor-pointer duration-200 border-b border-b-[#222] py-1 flex items-center
+                gap-x-3 hover:border-b-white group'>
+                    <span className='w-2 h-2 rounded-full inline-flex border border-red-700
+                    group-hover:bg-red-700 duration-200' />
+                    {item?.title}
+                </Link>
+            ))}
+        </div>
+    );
+}
 
 const Footer = () => {
     return (
@@ -19,6 +80,40 @@ const Footer = () => {
                         priority={true} className='cursor-pointer w-40 h-auto'
                     />
                 </Link>
+                <p className='text-gray-200 text-sm leading-6 tracking-wide mt-5 max-w-72'>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis vero totam quam inventore, magnam accusamus nihil ipsa eum accusantium voluptatem nobis sint laboriosam reprehenderit amet minus, ipsum mollitia soluta tempore.
+                </p>
+            </div>
+            <div>
+                <h2 className='text-base uppercase font-bold text-white
+                tracking-wide border-b border-b-gray-600 py-2 mb-5 relative'>
+                    Information <span className='w-16 h-1 bg-red-600 inline-block absolute left-0
+                    -bottom-[1.5px] z-10'/>
+                </h2>
+                <Information Array={infoArray} />
+            </div>
+            <div>
+                <h2 className='text-base uppercase font-bold text-white
+                tracking-wide border-b border-b-gray-600 py-2 mb-5 relative'>
+                    Category <span className='w-16 h-1 bg-red-600 inline-block absolute left-0
+                    -bottom-[1.5px] z-10'/>
+                </h2>
+                <Information Array={categoryArray} />
+            </div>
+            <div>
+                <h2 className='text-base uppercase font-bold text-white
+                tracking-wide border-b border-b-gray-600 py-2 mb-5 relative'>
+                    Connect With Us <span className='w-16 h-1 bg-red-600 inline-block absolute left-0
+                    -bottom-[1.5px] z-10'/>
+                </h2>
+                <div className='text-gray-300 text-sm flex flex-col gap-2'>
+                    <p>
+                        Phone: <span className='text-white font-medium'>001 7728 2269</span>
+                    </p>
+                    <p>
+                        Email: <span className='text-white font-medium'>kwnp17c@gmail.com</span>
+                    </p>
+                </div>
             </div>
         </div>
     )
